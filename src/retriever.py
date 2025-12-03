d60055b3220fc8c593cf1de09a607d1391c9c3f0
Sielecki Bogdan Radu Silviu <radu.sielecki@globalbrother.com>
2025-12-03 10:40:55 +0200

    [RAG_AGENTS][Add] Prepare for Demo

--- src/retriever.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.vectorstores import Chroma
-
-def load_retriever(persist_directory="vector_db"):
-    vectordb = Chroma(persist_directory=persist_directory)
-    retriever = vectordb.as_retriever(
-        search_type="similarity",
-        search_kwargs={"k": 4}
-    )
-    return retriever
